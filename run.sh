9f3b768eb8e1856d6c98af5e8e9c16fec5765ce2
Isaac Yonemoto <isaac@rstor.io>
2019-02-09 12:35:53 -0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd /script
+
+python3 setup.py build_ext --inplace
+
+./flow --imgdir $input_dir --output $output_dir --model cfg/yolov2.cfg --load bin/yolov2.weights --threshold 0.05 --json
