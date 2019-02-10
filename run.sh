afde11731bc156241f5ff135f05de953c1f39839
Isaac Yonemoto <isaac@rstor.io>
2019-02-09 17:13:15 -0800

    add a gpu option to run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,8 @@ cd /script
 
 python3 setup.py build_ext --inplace
 
-./flow --imgdir $input_dir --output $output_dir --model cfg/yolov2.cfg --load bin/yolov2.weights --threshold 0.05 --json
+if [ $gpu ]; then
+  ./flow --imgdir $input_dir --output $output_dir --model cfg/yolov2.cfg --load bin/yolov2.weights --threshold 0.05 --gpu 1.0 --json
+else
+  ./flow --imgdir $input_dir --output $output_dir --model cfg/yolov2.cfg --load bin/yolov2.weights --threshold 0.05 --json
+fi
